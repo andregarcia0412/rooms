7615bec642a092c9045f5e5f80b675f902cbda61
andregarcia0412 <andregarcia0412@gmail.com>
2026-02-08 17:03:36 -0300

    feature(backend): add StartupLogger

---
+++ backend/src/main/java/rooms/backend/infra/startup/StartupLogger.java
@@ -0,0 +1,22 @@
+package rooms.backend.infra.startup;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
+
+
+@Component
+public class StartupLogger {
+    private static final Logger log = LoggerFactory.getLogger(StartupLogger.class);
+
+    @Value("${server.port}")
+    private String port;
+
+    @EventListener(ApplicationReadyEvent.class)
+    public void onReady() {
+        log.info("✅ Service Running on port {}", port);
+    }
+}
